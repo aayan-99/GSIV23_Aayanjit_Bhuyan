fae8d2c9a0048a775bf5eaa96af33e1004b1e1f9
Aayanjit <aayanjit@ekoinfomatics.com>
2023-08-30 21:42:54 +0530

    initial commit

--- src/store/MoviesStore.ts
+++ src/store/MoviesStore.ts
@@ -2,17 +2,68 @@ import { makeObservable, observable } from "mobx";
 import { GetAllMoviesResponse, SingleMovieDetailsResponse } from "../types/movieDataTypes";
 
 export class MoviesStore {
-  allMovies: GetAllMoviesResponse
+  allMovies: GetAllMoviesResponse = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  }
 
-  popularMovies: GetAllMoviesResponse
+  popularMovies: GetAllMoviesResponse = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  }
 
-  topRatedMovies: GetAllMoviesResponse
+  topRatedMovies: GetAllMoviesResponse = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  }
 
-  latestMovies: GetAllMoviesResponse
+  latestMovies: GetAllMoviesResponse = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  }
 
-  singleMovieDetails: SingleMovieDetailsResponse
+  singleMovieDetails: SingleMovieDetailsResponse = {
+    adult: false,
+    backdrop_path: '',
+    belongs_to_collection: '',
+    budget: 0,
+    genres: [],
+    homepage: '',
+    id: 0,
+    imdb_id: '',
+    original_language: '',
+    original_title: '',
+    overview: '',
+    popularity: 0,
+    poster_path: '',
+    production_companies: [],
+    production_countries: [],
+    release_date: '',
+    revenue: 0,
+    runtime: 0,
+    spoken_languages: [],
+    status: '',
+    tagline: '',
+    title: '',
+    video: false,
+    vote_average: 0,
+    vote_count: 0,
+  }
 
-  searchResults: GetAllMoviesResponse
+  searchResults: GetAllMoviesResponse = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  }
 
   constructor() {
     makeObservable(this, {
